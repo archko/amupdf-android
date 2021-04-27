7e312ccd7551ea93a71e74726cdf2dc2a1ec58b0
archko <archko@gmail.com>
2021-04-27 21:59:40 +0800

    save as html

--- app/src/main/java/cn/archko/pdf/common/PDFCreaterHelper.kt
+++ app/src/main/java/cn/archko/pdf/common/PDFCreaterHelper.kt
@@ -1,17 +1,24 @@
 package cn.archko.pdf.common
 
+import android.content.Context
+import cn.archko.pdf.App
 import cn.archko.pdf.AppExecutors.Companion.instance
 import cn.archko.pdf.common.Logcat.d
+import cn.archko.pdf.mupdf.MupdfDocument
 import cn.archko.pdf.utils.FileUtils
+import cn.archko.pdf.utils.StreamUtils
 import com.artifex.mupdf.fitz.Buffer
 import com.artifex.mupdf.fitz.Image
 import com.artifex.mupdf.fitz.PDFDocument
 import com.artifex.mupdf.fitz.Rect
+import java.io.File
+import java.io.FileFilter
+import java.util.*
 
 /**
  * @author: archko 2018/12/21 :1:03 PM
  */
-class PDFCreaterHelper {
+object PDFCreaterHelper {
     var mDocument: PDFDocument? = null
     var imgname = "DCIM/Camera/IMG_20181221_125752.jpg"
     var filename = "test.pdf"
@@ -37,7 +44,62 @@ class PDFCreaterHelper {
         }
     }
 
-    companion object {
-        const val OPTS = "compress-images;compress;incremental;linearize;pretty;compress-fonts"
+    private val fileFilter: FileFilter = FileFilter { file ->
+        if (file.isDirectory) {
+            return@FileFilter false
+        }
+        val fname = file.name.toLowerCase(Locale.ROOT)
+        return@FileFilter fname.startsWith("Peter")
     }
+
+    fun getSdcard(): String {
+        var externalFileRootDir: File? = App.instance!!.getExternalFilesDir(null)
+        do {
+            externalFileRootDir = Objects.requireNonNull(externalFileRootDir)?.parentFile
+        } while (Objects.requireNonNull(externalFileRootDir)?.absolutePath?.contains("/Android") == true
+        )
+        val sdcardRoot = externalFileRootDir?.path!!
+        return sdcardRoot
+    }
+
+    fun saveBooksToHtml(context: Context) {
+        val sdcardRoot = getSdcard()
+        val dir = "$sdcardRoot/book/股票"
+
+        val files = File(dir).listFiles()
+        Logcat.d("saveBooksToHtml:$dir,${files.size}")
+        if (files != null) {
+            for (file in files) {
+                Logcat.d("saveBooksToHtml:$file")
+                if (file.isFile && file.name.startsWith("Peter")) {
+                    instance.diskIO().execute {
+                        kotlin.run { saveBookToHtml(context, sdcardRoot, file) }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun saveBookToHtml(context: Context, sdcardRoot: String, file: File) {
+        try {
+            val path = "$sdcardRoot/amupdf/${file.name}.html"
+            Logcat.d("save book:$path")
+            val mMupdfDocument = MupdfDocument(context)
+            mMupdfDocument.newDocument(file.absolutePath, null)
+            val cp: Int = mMupdfDocument.countPages()
+            val stringBuilder = StringBuilder()
+            for (i in 0 until cp) {
+                val loadPage = mMupdfDocument.loadPage(i)
+                val content =
+                    String(loadPage.textAsHtml2("preserve-whitespace,inhibit-spaces,preserve-images"))
+                stringBuilder.append(content)
+                loadPage.destroy()
+            }
+            StreamUtils.saveStringToFile(stringBuilder.toString(), path)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    const val OPTS = "compress-images;compress;incremental;linearize;pretty;compress-fonts"
 }
