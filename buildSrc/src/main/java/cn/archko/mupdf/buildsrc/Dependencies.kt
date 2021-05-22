d87584d7d9060bde2ed89b79c8e7087ca4d2b843
archko <archko@gmail.com>
2021-05-22 08:45:02 +0800

    remove iconsext update gradle plugin

--- buildSrc/src/main/java/cn/archko/mupdf/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/cn/archko/mupdf/buildsrc/Dependencies.kt
@@ -14,7 +14,7 @@ object BuildConfig {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-beta01"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-beta02"
     const val jdkDesugar = "com.android.tools:desugar_jdk_libs:1.0.9"
 
     object Accompanist {
