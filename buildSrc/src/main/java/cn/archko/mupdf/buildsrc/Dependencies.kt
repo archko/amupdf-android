52a864d636931d7a8de412a736b699232ad2edad
archko <archko@gmail.com>
2022-02-11 15:35:47 +0800

    update gradle , update compose to 1.1.0

--- buildSrc/src/main/java/cn/archko/mupdf/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/cn/archko/mupdf/buildsrc/Dependencies.kt
@@ -14,12 +14,12 @@ object BuildConfig {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.3"
     const val jdkDesugar = "com.android.tools:desugar_jdk_libs:1.1.5"
     const val flinger = "com.github.iamjosephmj:flinger:1.0.5"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.2.0-beta01"
 
     object Accompanist {
-        const val version = "0.22.0-rc"
+        const val version = "0.23.0"
         const val insets = "com.google.accompanist:accompanist-insets:$version"
         const val pager = "com.google.accompanist:accompanist-pager:$version"
         const val pagerIndicators = "com.google.accompanist:accompanist-pager-indicators:$version"
@@ -34,14 +34,14 @@ object Libs {
     }
 
     object Kotlin {
-        private const val version = "1.6.0"
+        private const val version = "1.6.10"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
         const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
     }
 
     object Coroutines {
-        private const val version = "1.5.2"
+        private const val version = "1.6.0"
         const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
         const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
         const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
@@ -55,11 +55,10 @@ object Libs {
 
     object AndroidX {
         const val coreKtx = "androidx.core:core-ktx:1.7.0"
-        const val navigation = "androidx.navigation:navigation-compose:2.4.0-beta01"
+        const val navigation = "androidx.navigation:navigation-compose:2.5.0-alpha01"
 
         object Compose {
-            const val snapshot = ""
-            const val version = "1.1.0-rc01"
+            const val version = "1.1.0"
 
             const val runtime = "androidx.compose.runtime:runtime:$version"
             const val animation = "androidx.compose.animation:animation:$version"
@@ -68,13 +67,19 @@ object Libs {
 
             const val ui = "androidx.compose.ui:ui:$version"
             const val material = "androidx.compose.material:material:$version"
-            const val material3 = "androidx.compose.material3:material3:1.0.0-alpha02"
             const val iconsExtended = "androidx.compose.material:material-icons-extended:$version"
 
             const val tooling = "androidx.compose.ui:ui-tooling:$version"
 
             const val uiUtil = "androidx.compose.ui:ui-util:$version"
             const val uiTest = "androidx.compose.ui:ui-test-junit4:$version"
+
+            object Material3 {
+                const val snapshot = ""
+                const val version = "1.0.0-alpha02"
+
+                const val material3 = "androidx.compose.material3:material3:$version"
+            }
         }
 
         object Activity {
@@ -100,31 +105,29 @@ object Libs {
 
         object ConstraintLayout {
             const val constraintLayoutCompose =
-                "androidx.constraintlayout:constraintlayout-compose:1.0.0-rc02"
+                "androidx.constraintlayout:constraintlayout-compose:1.0.0"
 
             const val constraintLayout =
-                "androidx.constraintlayout:constraintlayout:2.0.0-beta3"
-            const val constraintLayoutSolver =
-                "androidx.constraintlayout:constraintlayout-solver:2.0.0-beta3"
+                "androidx.constraintlayout:constraintlayout:2.1.3"
         }
 
         object Navigation {
-            private const val version = "2.4.0-rc01"
+            private const val version = "2.4.0"
             const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
             const val uiKtx = "androidx.navigation:navigation-ui-ktx:$version"
             const val runtime = "androidx.navigation:navigation-runtime:$version"
         }
 
         object Paging {
-            private const val version = "3.0.0"
+            private const val version = "3.1.0"
             const val pagingRuntime = "androidx.paging:paging-runtime:$version"
 
             // Jetpack Compose Integration
-            const val pagingCompose = "androidx.paging:paging-compose:1.0.0-alpha09"
+            const val pagingCompose = "androidx.paging:paging-compose:1.0.0-alpha14"
         }
 
         object Room {
-            private const val version = "2.4.0"
+            private const val version = "2.4.1"
             const val runtime = "androidx.room:room-runtime:${version}"
             const val ktx = "androidx.room:room-ktx:${version}"
             const val compiler = "androidx.room:room-compiler:${version}"
