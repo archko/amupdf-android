0a0bfe053fbdfacb9e0275ba547d66ea6017503e
archko <archko@gmail.com>
2022-07-12 15:41:44 +0800

    重构缩略图显示

--- app/src/main/java/cn/archko/pdf/ui/home/FileInfoDialog.kt
+++ app/src/main/java/cn/archko/pdf/ui/home/FileInfoDialog.kt
@@ -1,6 +1,5 @@
 package cn.archko.pdf.ui.home
 
-import android.graphics.Bitmap
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,19 +13,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.RememberObserver
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.painter.BitmapPainter
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -38,22 +27,12 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import cn.archko.mupdf.R
 import cn.archko.pdf.App
-import cn.archko.pdf.common.ImageLoader
 import cn.archko.pdf.common.ImageWorker
 import cn.archko.pdf.components.Divider
 import cn.archko.pdf.entity.FileBean
 import cn.archko.pdf.utils.DateUtils
 import cn.archko.pdf.utils.FileUtils
 import cn.archko.pdf.utils.Utils
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.mapLatest
-import kotlinx.coroutines.launch
 import java.math.BigDecimal
 
 @Composable
@@ -267,7 +246,7 @@ fun FileInfoDialog(
                             }*/
                             Image(
                                 painter = remember {
-                                    AsyncImagePainter(
+                                    AsyncPdfImagePainter(
                                         ImageWorker.DecodeParam(
                                             fileBean.file!!.path,
                                             0,
@@ -291,133 +270,4 @@ fun FileInfoDialog(
         }
     }
 
-}
-
-class AsyncImagePainter internal constructor(
-    request: ImageWorker.DecodeParam,
-) : Painter(), RememberObserver {
-
-    private var rememberScope: CoroutineScope? = null
-    private val drawSize = MutableStateFlow(Size.Zero)
-
-    private var painter: Painter? by mutableStateOf(null)
-    private var _state: State = State.Empty
-        set(value) {
-            field = value
-            state = value
-        }
-    private var _painter: Painter? = null
-        set(value) {
-            field = value
-            painter = value
-        }
-
-    internal var transform = DefaultTransform
-    internal var onState: ((State) -> Unit)? = null
-    var state: State by mutableStateOf(State.Empty)
-        private set
-
-    var request: ImageWorker.DecodeParam by mutableStateOf(request)
-        internal set
-
-    override val intrinsicSize: Size
-        get() = painter?.intrinsicSize ?: Size.Unspecified
-
-    override fun DrawScope.onDraw() {
-        drawSize.value = size
-
-        painter?.apply { draw(size) }
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    override fun onRemembered() {
-        if (rememberScope != null) return
-
-        val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
-        rememberScope = scope
-
-        (_painter as? RememberObserver)?.onRemembered()
-
-        scope.launch {
-            snapshotFlow {
-                val bitmap: Bitmap? = ImageLoader.decodeFromPDF(
-                    request.key,
-                    request.pageNum,
-                    request.zoom,
-                    request.screenWidth
-                )
-                bitmap
-            }.flowOn(Dispatchers.IO)
-                .mapLatest { bitmap ->
-                    if (null != bitmap) {
-                        State.Success(
-                            BitmapPainter(
-                                bitmap.asImageBitmap(),
-                            ), bitmap
-                        )
-                    } else {
-                        State.Error(painter, "null")
-                    }
-                }
-                .collect(::updateState)
-        }
-    }
-
-    override fun onForgotten() {
-        clear()
-        (_painter as? RememberObserver)?.onForgotten()
-    }
-
-    override fun onAbandoned() {
-        clear()
-        (_painter as? RememberObserver)?.onAbandoned()
-    }
-
-    private fun clear() {
-        rememberScope?.cancel()
-        rememberScope = null
-    }
-
-    private fun updateState(input: State) {
-        val previous = _state
-        val current = transform(input)
-        //Logcat.d("updateState.current:${current}, $_state, $_painter")
-        _state = current
-        _painter = current.painter
-
-        // Manually forget and remember the old/new painters if we're already remembered.
-        if (rememberScope != null && previous.painter !== current.painter) {
-            (previous.painter as? RememberObserver)?.onForgotten()
-            (current.painter as? RememberObserver)?.onRemembered()
-        }
-
-        onState?.invoke(current)
-    }
-
-    sealed class State {
-
-        abstract val painter: Painter?
-
-        object Empty : State() {
-            override val painter: Painter? get() = null
-        }
-
-        data class Loading(
-            override val painter: Painter?,
-        ) : State()
-
-        data class Success(
-            override val painter: Painter?,
-            val result: Bitmap,
-        ) : State()
-
-        data class Error(
-            override val painter: Painter?,
-            val result: String,
-        ) : State()
-    }
-
-    companion object {
-        val DefaultTransform: (State) -> State = { it }
-    }
 }
