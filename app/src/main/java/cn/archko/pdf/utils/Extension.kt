73f1607ea73f72e479b93d79f210a502a85e6143
archko <archko@gmail.com>
2022-08-26 16:54:52 +0800

    修改图标

--- app/src/main/java/cn/archko/pdf/utils/Extension.kt
+++ app/src/main/java/cn/archko/pdf/utils/Extension.kt
@@ -9,7 +9,8 @@ import java.util.*
  * @author: archko 2021/4/14 :16:25
  */
 fun FileBean.getIcon(): Int {
-    var iconId = R.drawable.ic_explorer_fldr
+    //var iconId = R.drawable.ic_explorer_fldr
+    var iconId = R.drawable.sodk_editor_icon_folder
     if (type == FileBean.HOME || type == FileBean.CURRENT) {
     } else if (type == FileBean.NORMAL && isDirectory && !isUpFolder) {
     } else if (isUpFolder) {
@@ -19,15 +20,22 @@ fun FileBean.getIcon(): Int {
                 val ext: String = bookProgress!!.ext!!.toLowerCase(Locale.ROOT)
 
                 if (ext.contains("pdf")) {
-                    iconId = R.drawable.ic_item_book
+                    //iconId = R.drawable.ic_item_book
+                    iconId = R.drawable.sodk_editor_icon_pdf
                 } else if (ext.contains("epub")) {
-                    iconId = R.drawable.ic_item_book
+                    //iconId = R.drawable.ic_item_book
+                    iconId = R.drawable.sodk_editor_icon_epub
                 } else if (ext.contains("mobi")) {
-                    iconId = R.drawable.ic_item_book
-                } else if (ext.contains("txt")) {
-                    iconId = R.drawable.ic_item_book
+                    //iconId = R.drawable.ic_item_book
+                    iconId = R.drawable.sodk_editor_icon_epub
+                } else if (ext.contains("txt") || ext.contains("log")) {
+                    //iconId = R.drawable.ic_item_book
+                    iconId = R.drawable.sodk_editor_icon_txt
+                } else if (ext.contains("png") || ext.contains("jpg")) {
+                    iconId = R.drawable.sodk_editor_icon_image
                 } else {
-                    iconId = R.drawable.ic_explorer_any
+                    //iconId = R.drawable.ic_explorer_any
+                    iconId = R.drawable.sodk_editor_icon_any
                 }
             }
         }
