4a7358c67e03a030cd46ea7771f1196b386f9c5e
archko <archko@sina.com>
2024-03-25 16:39:39 +0800

    减少缓存的操作,比较耗时,因为用到了同步

--- vudroid/src/main/java/org/vudroid/core/PageTreeNode.java
+++ vudroid/src/main/java/org/vudroid/core/PageTreeNode.java
@@ -101,8 +101,8 @@ public class PageTreeNode {
         }
         Bitmap bmp = getBitmap();
         if (bmp != null && !bmp.isRecycled()) {
-            //System.out.println(String.format("level:%s, page:%s, width:%s, %s", treeNodeDepthLevel, page.index, getBitmap().getWidth(), getBitmap().getHeight()));
-            canvas.drawBitmap(bmp, new Rect(0, 0, getBitmap().getWidth(), getBitmap().getHeight()), getTargetRect(), bitmapPaint);
+            //System.out.println(String.format("level:%s, page:%s, width:%s, %s", treeNodeDepthLevel, page.index, bmp.getWidth(), bmp.getHeight()));
+            canvas.drawBitmap(bmp, new Rect(0, 0, bmp.getWidth(), bmp.getHeight()), getTargetRect(), bitmapPaint);
             //canvas.drawRect(getTargetRect(), strokePaint);
         }
         if (children == null) {
@@ -237,7 +237,7 @@ public class PageTreeNode {
 
     private void release() {
         if (bitmap != null) {
-            BitmapCache.getInstance().remove(getCacheKey());
+            //BitmapCache.getInstance().remove(getCacheKey());
             BitmapPool.getInstance().release(bitmap);
             bitmapWeakReference.clear();
             bitmap = null;
