c7aec8fbc0fa85d09b6daf51aa9ce2796eba4fca
archko <archko@sina.com>
2024-12-17 16:58:06 +0800

    版本更新 隐藏搜索,目前so在有些机器上会崩溃

--- reader/src/main/java/cn/archko/pdf/controller/DefaultPageController.java
+++ reader/src/main/java/cn/archko/pdf/controller/DefaultPageController.java
@@ -85,6 +85,7 @@ public abstract class DefaultPageController implements IPageController, View.OnC
         //closeBtn.setOnClickListener(this);
         searchButton.setOnClickListener(this);
         //showSearchButton.setOnClickListener(this);
+        searchButton.setVisibility(View.GONE);
 
         mPageSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             public void onStopTrackingTouch(SeekBar seekBar) {
