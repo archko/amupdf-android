ec8856dedf26286ddd521e11e2fcc63b0db1e4a2
archko <archko@sina.com>
2025-09-25 10:09:07 +0800

    去除epub的边距

--- vudroid/src/main/java/org/vudroid/epub/codec/FontCSSGenerator.kt
+++ vudroid/src/main/java/org/vudroid/epub/codec/FontCSSGenerator.kt
@@ -22,25 +22,31 @@ object FontCSSGenerator {
             appendLine("    font-family: '$fontFamily' !important;")
             appendLine("    src: url('file://$fontPath');")
             appendLine("}")
-            appendLine()
+
             appendLine("body {")
             appendLine("    font-family: '$fontFamily', serif !important;")
+            appendLine("    margin: 30px !important; padding: 0 !important;")
             appendLine("}")
-            appendLine()
+
             appendLine("p, div, span, h1, h2, h3, h4, h5, h6, calibre, calibre1, calibre2, calibre3, calibre4, calibre5, calibre6, contents, contents1 {")
             appendLine("    font-family: '$fontFamily', serif !important;")
             appendLine("}")
+
+            // 强制所有元素的边距和内边距都为 0
+            appendLine("* {")
+            appendLine("    margin: 0 !important;")
+            appendLine("    padding: 0 !important;")
+            appendLine("}")
+            appendLine("    @page { margin: 0 !important; }")
+            appendLine("    p { margin: 0 !important; padding: 0 !important; }")
+            appendLine("    blockquote { margin: 0 !important; padding: 0 !important; }")
+            appendLine()
         }
     }
-
-//        appendLine("body { font-family: 'SimSun', serif; }")
-//        appendLine("h1, h2, h3, h4, h5, h6 { font-family: 'SimSun', serif; }")
-//        appendLine("pre, code { font-family: 'SimHei', monospace; }")
 
     private fun getFontNameFromPath(fontPath: String): String {
         val fileName = File(fontPath).name
         val dotIndex = fileName.lastIndexOf('.')
         return if (dotIndex > 0) fileName.substring(0, dotIndex) else fileName
     }
-
 }
