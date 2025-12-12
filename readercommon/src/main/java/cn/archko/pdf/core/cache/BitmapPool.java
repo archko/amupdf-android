e932a997e3271d91b4d4053db190b527a8696214
archko <archko@sina.com>
2025-12-12 17:24:05 +0800

    添加同步,避免段错误

--- readercommon/src/main/java/cn/archko/pdf/core/cache/BitmapPool.java
+++ readercommon/src/main/java/cn/archko/pdf/core/cache/BitmapPool.java
@@ -105,39 +105,38 @@ public class BitmapPool {
             mPool = new Object[maxPoolSize];
         }
 
-        @Override
-        @SuppressWarnings("unchecked")
-        public T acquire() {
-            if (mPoolSize > 0) {
-                final int lastPooledIndex = mPoolSize - 1;
-                T instance = (T) mPool[lastPooledIndex];
-                mPool[lastPooledIndex] = null;
-                mPoolSize--;
-                return instance;
-            }
-            return null;
+    @Override
+    @SuppressWarnings("unchecked")
+    public synchronized T acquire() {
+        if (mPoolSize > 0) {
+            final int lastPooledIndex = mPoolSize - 1;
+            T instance = (T) mPool[lastPooledIndex];
+            mPool[lastPooledIndex] = null;
+            mPoolSize--;
+            return instance;
         }
+        return null;
+    }
 
-        @Override
-        public boolean release(@NonNull T instance) {
-            if (isInPool(instance)) {
+    @Override
+    public synchronized boolean release(@NonNull T instance) {
+        if (isInPool(instance)) {
+            return true;
+        }
+        if (mPoolSize < mPool.length) {
+            mPool[mPoolSize] = instance;
+            mPoolSize++;
+            return true;
+        }
+        return false;
+    }
+
+    private synchronized boolean isInPool(@NonNull T instance) {
+        for (int i = 0; i < mPoolSize; i++) {
+            if (mPool[i] == instance) {
                 return true;
             }
-            if (mPoolSize < mPool.length) {
-                mPool[mPoolSize] = instance;
-                mPoolSize++;
-                return true;
-            }
-            return false;
         }
-
-        private boolean isInPool(@NonNull T instance) {
-            for (int i = 0; i < mPoolSize; i++) {
-                if (mPool[i] == instance) {
-                    return true;
-                }
-            }
-            return false;
-        }
+        return false;
     }
 }
